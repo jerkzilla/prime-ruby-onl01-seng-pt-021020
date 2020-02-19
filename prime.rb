c6b0a522c0fddaa395ebe514f5369088726bdb70
John J Marshall Jr <johnjamesmarshalljr@gmail.com>
2020-02-19 23:39:57 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,16 @@
+require 'pry'
+
 def prime?(integer)
     i = 2
     if integer.negative? || integer < 2
       return false
     elsif i < integer
-        return false if integer % i == 0
+      binding.pry
+      i..integer - 1 
+        return false if integer % i == 0 
         i += 1
     end
     true
 end
+
+
