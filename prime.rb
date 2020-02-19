bcee67bab2b9a82c60b196bb387dc77a18459fe6
John J Marshall Jr <johnjamesmarshalljr@gmail.com>
2020-02-19 22:48:18 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,10 @@
 def prime?(integer)
-    i = 2
+    i = 1
     if integer.negative? || 0
       return false
     elsif i < integer
         return false if integer % i == 0 && integer < 1
         i += 1
     end
+    true
 end
