e1040bd1c39cc06d5860b789973d67bd40a950f6
John J Marshall Jr <johnjamesmarshalljr@gmail.com>
2020-02-19 22:39:01 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,10 @@
-# Add  code here!
+def prime?(integer)
+    i = 2
+    if integer.negative? || 0
+      return false
+    elsif i < integer
+        return false if integer % i == 0 && integer < 1
+        i += 1
+    end
+    true
+end
